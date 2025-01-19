8c5638915cd748376425c4fb3dfea37c495d85b1
yasmin <yasminrhamouda@hotmail.com>
2025-01-19 12:20:14 +0000

    added NavItem content

--- src/components/navbar/NavItem.tsx
+++ src/components/navbar/NavItem.tsx
@@ -4,6 +4,8 @@ import { PRODUCT_CATEGORIES } from "@/config";
 import { Button } from "../ui/button";
 import { ChevronDownIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 type Category = typeof PRODUCT_CATEGORIES[number];
 
@@ -26,6 +28,33 @@ const NavItem = ({isOpen, isAnyOpen, handleOpen, category}: NavItemProps) => {
                         "-rotate-180": isOpen,
                     })} />
                 </Button>
+            </div>
+            <div
+                className={cn("absolute inset x-0 top-full text-sm text-muted-foreground",
+                    {
+                        "animte-in fade-in-10 slide-in-from-top-5" :
+                            !isAnyOpen,
+                    }
+            )}>
+            <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
+
+            <div className="mx-auto max-w-7xl px-8">
+                <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+                    <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+                        {category.featured.map((item) => (
+                            <div key={item.name} className="group relative text-base sm:text-sm">
+                                <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                                    <Image src={item.imageSrc} alt="produc category image" fill className="object-cover object-center" />
+                                </div>
+
+                                <Link href={item.href} className="mt-6 block font-medium text-gray-900">
+                                <p className="mt-1"
+                                aria-hidden="true">Shop now</p></Link>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     );
