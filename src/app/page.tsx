244eae0b4efc208995d1a807f6d892271a3f888c
yasmin <yasminrhamouda@hotmail.com>
2025-01-19 09:55:44 +0000

    fixed h1 typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1>
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
           Your marketplace for high quality{" "}
           <span className="text-blue-600">digital assets</span>
+          .
         </h1>
       </div>
     </MaxWidthWrapper>
