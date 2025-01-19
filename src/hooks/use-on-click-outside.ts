715587181bae51331288c27aac86adbf9e228328
yasmin <yasminrhamouda@hotmail.com>
2025-01-19 12:51:28 +0000

    accept RefObject<HTMLElement | null> to handle cases when ref can be null

--- src/hooks/use-on-click-outside.ts
+++ src/hooks/use-on-click-outside.ts
@@ -3,7 +3,8 @@ import { RefObject, useEffect } from "react";
 type Event = MouseEvent | TouchEvent;
 
 export const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
-  ref: RefObject<T>,
+  ref: RefObject<T | null>,  // Allow null explicitly
+
   handler: (event: Event) => void
 ) => {
   useEffect(() => {
