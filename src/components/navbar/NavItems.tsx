aef12b5f01f896895219cf8fc5fac7de64b2189d
yasmin <yasminrhamouda@hotmail.com>
2025-01-19 12:42:51 +0000

    added hook (keyboard close) to for Navmenu

--- src/components/navbar/NavItems.tsx
+++ src/components/navbar/NavItems.tsx
@@ -1,39 +1,63 @@
-// make use of state (interactivitiy), which we cannot do with server components
-"use client";
-import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
-import NavItem from "./NavItem";
+'use client'
+
+import { PRODUCT_CATEGORIES } from '@/config'
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
+import { useEffect, useRef, useState } from 'react'
+import NavItem from './NavItem'
 
 const NavItems = () => {
-  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const [activeIndex, setActiveIndex] = useState<
+    null | number
+  >(null)
 
-  const isAnyOpen = activeIndex !== null;
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null)
+      }
+    }
+
+    document.addEventListener('keydown', handler)
+
+    return () => {
+      document.removeEventListener('keydown', handler)
+    }
+  }, [])
+
+  const isAnyOpen = activeIndex !== null
+
+  const navRef = useRef<HTMLDivElement | null>(null)
+
+  useOnClickOutside(navRef, () => setActiveIndex(null))
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className='flex gap-4 h-full' ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
-        // keep track of which item is currently opened and which one is not
         const handleOpen = () => {
           if (activeIndex === i) {
-            setActiveIndex(null);
+            setActiveIndex(null)
           } else {
-            setActiveIndex(i);
+            setActiveIndex(i)
           }
-        };
+        }
 
-        const isOpen = activeIndex === i;
+        const close = () => setActiveIndex(null)
+
+        const isOpen = i === activeIndex
 
         return (
           <NavItem
             category={category}
+            close={close}
             handleOpen={handleOpen}
             isOpen={isOpen}
-            isAnyOpen={isAnyOpen}
             key={category.value}
+            isAnyOpen={isAnyOpen}
           />
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default NavItems;
+export default NavItems
