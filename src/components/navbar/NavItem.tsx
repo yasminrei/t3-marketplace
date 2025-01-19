a80e59cb73c53e8ed3ec517cafb86e8bad832145
yasmin <yasminrhamouda@hotmail.com>
2025-01-19 11:47:35 +0000

    added NavItem component and its interactivity

---
+++ src/components/navbar/NavItem.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { Button } from "../ui/button";
+import { ChevronDownIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+type Category = typeof PRODUCT_CATEGORIES[number];
+
+interface NavItemProps {
+    category: Category;
+    handleOpen: () => void;
+    isOpen: boolean;
+    isAnyOpen: boolean;
+}
+
+const NavItem = ({isOpen, isAnyOpen, handleOpen, category}: NavItemProps) => {
+    return (
+        <div className="flex">
+            <div className="relative flex items-center">
+                <Button className="gap-1.5" onClick={handleOpen} variant={isOpen ? "secondary" : "ghost"}>
+                    {category.label}
+                    <ChevronDownIcon 
+                        className={cn("h-4 w-4 transition-all text-muted-foreground", 
+                    {
+                        "-rotate-180": isOpen,
+                    })} />
+                </Button>
+            </div>
+        </div>
+    );
+}                    
+
+export default NavItem;
