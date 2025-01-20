238b91081247f4fdddc890db13ff40e13a7539d1
yasmin <yasminrhamouda@hotmail.com>
2025-01-20 02:02:38 +0000

    create format price function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,20 +5,32 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+// Take a price and target currency
+
 export function formatPrice(
-  price: number | string, 
+  price: number | string,
   options: {
-    currency?: "USD | EUR | GBP | JPY | INR | AUD | CAD",
+    currency?: "USD" | "EUR" | "GBP" | "JPY" | "INR" | "AUD" | "CAD"
+    // notation is going to be "compact" by default
+    // take an optional parameter for the notation
     notation?: Intl.NumberFormatOptions["notation"]
-  } = {}) {
-    const { currency = "USD", notation = "compact" } = options
+  } = {}
+) {
+  // default currency and notation is USD and compact
+  const { currency = "USD", notation = "compact" } = options
 
-    const numericPrice = typeof price === "string" ? parseFloat(price) : price
+  // before we format the price, we need to convert it to a number
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price
 
-    return Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency,
-      notation,
-      maximumFractionDigits: 2,
-    }).format(numericPrice)
+  // return the formatted price using the passed price and currency using Intl.NumberFormat
+
+
+  //create a formatting object to format the price
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
 }
