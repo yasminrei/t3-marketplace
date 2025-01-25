d2ae06dfbd2cdd721ae5cd553e51c99cef782471
yasmin <yasminrhamouda@hotmail.com>
2025-01-25 21:58:42 +0000

    add empty cart image and message

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,10 +6,11 @@ import { Separator } from "./ui/separator";
 import { formatPrice } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 const Cart = () => {
 
-  const itemCount = 1;
+  const itemCount = 0;
   const fee = 1;
 
   // calculate total price
@@ -60,16 +61,37 @@ const Cart = () => {
                 Continue to Checkout
               </Link>
             </SheetTrigger>
-
             </SheetFooter>
           </div>
         </>
       ) : (
-        // if itemcount is 0, render image in place of cart
-      <div>
-        
-      </div>
-    )}
+        <div className='flex h-full flex-col items-center justify-center space-y-1'>
+          <div
+            aria-hidden='true'
+            className='relative mb-4 h-60 w-60 text-muted-foreground'>
+            <Image
+              src='/hippo-empty-cart.png'
+              fill
+              alt='empty shopping cart hippo'
+            />
+          </div>
+          <div className='text-xl font-semibold'>
+            Your cart is empty
+          </div>
+          <SheetTrigger asChild>
+            <Link
+              href='/products'
+              className={buttonVariants({
+                variant: 'link',
+                size: 'sm',
+                className:
+                  'text-sm text-muted-foreground',
+              })}>
+              Add items to your cart to checkout
+            </Link>
+          </SheetTrigger>
+        </div>
+      )}
     </SheetContent>
     
   </Sheet>
